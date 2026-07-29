50172795b8c8d74c5b27233ce9ddf6e5aca08d09
Qt159 <tuan.pham2005@icloud.com>
2026-07-29 19:33:22 +0700

    update ListCourse + diagram

--- src/main/java/com/tuan/service/CourseService.java
+++ src/main/java/com/tuan/service/CourseService.java
@@ -7,12 +7,12 @@ import java.util.List;
 public class CourseService {
     public static final List<CourseBean> courseList = new ArrayList<>();
     static {
-        courseList.add(new CourseBean("MTH10334", "Lập trình Web với J2EE", 4, "Thầy Võ Đức Cẩm Hải", 30, 4000000));
-        courseList.add(new CourseBean("MTH10360", "Lập trình Java", 4, "Thầy Hà Văn Thảo", 30, 4000000));
-        courseList.add(new CourseBean("MTH10323", "Xử lý đa chiều", 4, "Thầy A", 30, 4000000));
-        courseList.add(new CourseBean("MTH10356", "Trí tuệ nhân tạo nâng cao", 4, "Thầy B", 30, 4000000));
-        courseList.add(new CourseBean("MTH10605", "Python cho khoa học dữ liệu", 3, "Cô C", 30, 3000000));
-        courseList.add(new CourseBean("MTH10624", "Phân tích mạng xã hội", 3, "Thầy D", 30, 3000000));
+        courseList.add(new CourseBean("MTH10334", "Lập trình Web với J2EE", 4, "Giảng viên A", 30, 4000000));
+        courseList.add(new CourseBean("MTH10360", "Lập trình Java", 4, "Giảng viên B", 30, 4000000));
+        courseList.add(new CourseBean("MTH10323", "Xử lý đa chiều", 4, "Giảng viên C", 30, 4000000));
+        courseList.add(new CourseBean("MTH10356", "Trí tuệ nhân tạo nâng cao", 4, "Giảng viên D", 30, 4000000));
+        courseList.add(new CourseBean("MTH10605", "Python cho khoa học dữ liệu", 3, "Giảng viên E", 30, 3000000));
+        courseList.add(new CourseBean("MTH10624", "Phân tích mạng xã hội", 3, "Giảng viên F", 30, 3000000));
     }
 
     public List<CourseBean> searchCourses(String keyword) {
